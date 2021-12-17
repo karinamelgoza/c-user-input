c80ba99bd2f60a517cccae02eec1a302cc26da62
Karina Melgoza <kmelgoza08@gmail.com>
2021-12-17 12:21:50 -0700

    initial commit

---
+++ UserInput/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+            Console.Read();
+        }
+    }
+}
