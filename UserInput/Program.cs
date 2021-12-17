35b140cc13889f13b323c31f63d30844aac92ff7
Karina Melgoza <kmelgoza08@gmail.com>
2021-12-17 12:41:07 -0700

    Adds practice with user input in methods

--- UserInput/Program.cs
+++ UserInput/Program.cs
@@ -6,9 +6,16 @@ namespace UserInput
     {
         static void Main(string[] args)
         {
-            string input = Console.ReadLine();
-            Console.WriteLine(input);
-            Console.Read();
+            Console.WriteLine($"Total: {Add()}");
+        }
+
+        public static int Add()
+        {
+            Console.Write("Enter a number: ");
+            string num1 = Console.ReadLine();
+            Console.Write("Enter another number: ");
+            string num2 = Console.ReadLine();
+            return int.Parse(num1) + int.Parse(num2);
         }
     }
 }
